fa134b86c3e959ec81881a8eaea3e530f956341a
Sky Kosiner <sky@skykosiner.co.uk>
2023-04-09 11:30:13 +0000

    feat: Done everything I wanted it to do, now time to write documentain yay

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,9 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/atotto/clipboard"
+	"github.com/ktr0731/go-fuzzyfinder"
 	"github.com/skykosiner/find-me-daddy/pkg/clip"
-	"golang.design/x/clipboard"
 )
 
 func main() {
@@ -16,40 +17,68 @@ func main() {
 	args := os.Args[1:]
 
 	switch args[0] {
-	case "--add":
+	case "-add":
 		c.AddToHistory()
-	case "--get-list":
+	case "-get-list":
 		for _, item := range c.GetItems() {
 			fmt.Println(item)
 		}
-	case "--get-fuzzy":
+	case "-get-fuzzy":
 		items := c.GetItems()
 		searcher := os.Getenv("FIND_ME_DADDY_FUZZY")
 
-		// If the user has not sellected anything just use dmenu (this only
-		// works on Linux, as Mac L, and windows even bigger L)
 		if searcher == "" {
 			searcher = "dmenu"
 		}
 
-		fmt.Println("seacher", searcher)
+		if len(args) > 1 {
+			switch args[1] {
+			case "-f":
+				searcher = "fzf"
+			case "-d":
+				searcher = "dmenu"
+			case "-r":
+				searcher = "rofi -dmenu"
+			default:
+				searcher := os.Getenv("FIND_ME_DADDY_FUZZY")
 
-		// TODO: WHY WON"T THIS WORK???????
-		cmd := fmt.Sprintf("echo '%s' | sed 's/\\[//' | sed 's/\\]//' | sed 's/^$//' | %s ", items, searcher)
-		output := exec.Command("bash", "-c", cmd)
-
-		stdOut, err := output.Output()
-
-		if err != nil {
-			log.Fatal("Error with finding the daddy ", err)
+				if searcher == "" {
+					searcher = "dmenu"
+				}
+			}
 		}
 
-		err = clipboard.Init()
-		if err != nil {
-			log.Fatal("Error with clipboard ", err)
+		if searcher == "fzf" {
+			idx, err := fuzzyfinder.FindMulti(
+				items,
+				func(i int) string {
+					return items[i]
+				},
+			)
+
+			if err != nil {
+				log.Fatal("Error fuzzy finding", err)
+			}
+
+			err = clipboard.WriteAll(items[idx[0]])
+			if err != nil {
+				log.Fatal("Error wirting to clipboard", err)
+			}
+		} else {
+			cmd := fmt.Sprintf("echo '%s' | sed 's/\\[//' | sed 's/\\]//' | sed 's/^$//' | %s ", items, searcher)
+			output := exec.Command("bash", "-c", cmd)
+
+			stdOut, err := output.Output()
+
+			if err != nil {
+				log.Fatal("Error with finding the daddy ", err)
+			}
+
+			err = clipboard.WriteAll(string(stdOut))
+			if err != nil {
+				log.Fatal("Error wirting to clipboard", err)
+			}
 		}
 
-		fmt.Println(string(stdOut))
-		clipboard.Write(clipboard.FmtText, []byte("test"))
 	}
 }
--- go.mod
+++ go.mod
@@ -5,14 +5,21 @@ go 1.20
 require github.com/charmbracelet/bubbletea v0.23.2
 
 require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.5.3 // indirect
+	github.com/ktr0731/go-ansisgr v0.1.0 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.6.0 // indirect
 	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4
 	github.com/aymanbagabas/go-osc52 v1.2.1 // indirect
 	github.com/containerd/console v1.0.3 // indirect
+	github.com/ktr0731/go-fuzzyfinder v0.7.0
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -21,7 +28,7 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.14.0 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
 	golang.design/x/clipboard v0.7.0
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
